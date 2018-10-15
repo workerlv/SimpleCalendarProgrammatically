8daa1abca178fe0c04aca50db1c84f945ba8c971
Workerlv <arturs280@inbox.lv>
2018-10-15 22:18:53 +0300

    Initial Commit

---
+++ Calendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Arturs Vitins on 15/10/2018.
+//  Copyright © 2018 Arturs Vitins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
